a19c4d94cb63b50080c6dc099502411fed6ff051
Daniel Augusto Muñoz Viveros <danielvm@unicauca.edu.co>
2021-09-09 12:01:06 -0500

    About_Us_finished

--- src/components/templates/About/About.js
+++ src/components/templates/About/About.js
@@ -3,6 +3,7 @@ import Develop from "../../../assets/images/IconServicesProducts/desarrollador_S
 import CRM from "../../../assets/images/IconServicesProducts/crm.svg";
 import infra from "../../../assets/images/IconServicesProducts/estructure.svg";
 import OwnProducts from "../../../assets/images/IconServicesProducts/ownProducts.svg";
+import Cali from "../../../assets/images/BackGrounds/Cali_City.mp4";
 import "./About.scss";
 
 export const About = () => {
@@ -56,7 +57,20 @@ export const About = () => {
 
       <div id="AboutSection1" className="custom-content-section">
         <div className="custom-row">
-          <div className="custom-image-container_1"></div>
+          <div className="custom-image-container_1"><video
+          className="video"
+          preload="auto"
+          autoplay="true"
+          loop="loop"
+          aria-hidden="true"
+          playsinline="true"
+          muted
+          volume="0"
+          width="600" 
+          height="350"
+        >
+          <source src={Cali} type="video/mp4" />
+        </video></div>
           <div className="custom-content" style={{ flex: "0.7" }}>
             <span
               className="custom-title-span"
