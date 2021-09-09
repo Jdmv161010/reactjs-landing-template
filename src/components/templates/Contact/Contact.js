51e007c5f1918df0e4ada504f9506da0a144ed37
Daniel Augusto Muñoz Viveros <danielvm@unicauca.edu.co>
2021-09-09 17:43:23 -0500

    Bing_Map

--- src/components/templates/Contact/Contact.js
+++ src/components/templates/Contact/Contact.js
@@ -1,10 +1,23 @@
 import React from "react";
+import BingMapsReact from "bingmaps-react";
 import { Carousel } from "../../molecules/Carousel/Carousel";
 
 export const Contact = () => {
   return (
     <div className="custom-container">
-      <Carousel />
+      {/* <Carousel /> */}
+      <BingMapsReact
+        bingMapsKey="AmN3jZxAlbTHAQzmy5ondBHzpwgHTXmnbxCOEYPsJysWjexkownz6VJpu_NzQ0cR"
+        height="500px"
+        mapOptions={{
+          navigationBarMode: "square",
+        }}
+        width="500px"
+        viewOptions={{
+          center: { latitude: 3.4741799613928137, longitude: -76.52160245975595 },
+          // mapTypeId: "grayscale", 
+        }}
+        />
     </div>
   );
 };
